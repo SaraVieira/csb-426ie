537c7aa42da56b30a457739ef24defeed19e4af5
Code Sandbox <hello@codesandbox.io>
2021-03-11 10:29:16 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f5350aaa5fc538300a3360ad3d03aa58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb2a6ff00f7aa3012e42e677141c413",
+    "url": "https://SaraVieira.github.io/csb-426ie/index.html"
+  },
+  {
+    "revision": "7241ba62b10327e57f15",
+    "url": "https://SaraVieira.github.io/csb-426ie/static/css/main.16122306.chunk.css"
+  },
+  {
+    "revision": "331cb6c44d7b291c91a1",
+    "url": "https://SaraVieira.github.io/csb-426ie/static/js/2.f0bc85ff.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "https://SaraVieira.github.io/csb-426ie/static/js/2.f0bc85ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7241ba62b10327e57f15",
+    "url": "https://SaraVieira.github.io/csb-426ie/static/js/main.3143baeb.chunk.js"
+  },
+  {
+    "revision": "f910650f529de210b81e",
+    "url": "https://SaraVieira.github.io/csb-426ie/static/js/runtime-main.84073c63.js"
+  }
+]);
